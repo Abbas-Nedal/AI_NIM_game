f29ce9ad589a5f8bfe4569042fd6782ea06c30c8
Abbas-Nedal <s12011080@stu.najah.edu>
2024-02-27 16:32:56 +0200

    NIm game

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.aigame {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.aigame to javafx.fxml;
+    exports com.example.aigame;
+}
